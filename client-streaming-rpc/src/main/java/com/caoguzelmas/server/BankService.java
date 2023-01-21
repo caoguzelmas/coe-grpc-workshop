71563646df51a364ea54f90f67f4abaaca531446
caoguzelmas <caoguzelmas@gmail.com>
2023-01-21 03:35:40 +0300

    client-streaming-rpc added

---
+++ client-streaming-rpc/src/main/java/com/caoguzelmas/server/BankService.java
@@ -0,0 +1,47 @@
+package com.caoguzelmas.server;
+
+import com.caoguzelmas.models.*;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+
+public class BankService extends BankServiceGrpc.BankServiceImplBase {
+
+    @Override
+    public void getBalance(BalanceCheckRequest request, StreamObserver<Balance> responseObserver) {
+
+        Balance balance = Balance.newBuilder()
+                .setAmount(AccountDatabase.getBalance(request.getAccountNumber()))
+                .build();
+
+        responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withDraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+        int accountNumber = request.getAccountNumber();
+        int amount = request.getAmount();
+        int balance = AccountDatabase.getBalance(accountNumber);
+
+        if (balance < amount) {
+            // status codes doc
+            // https://developers.google.com/maps-booking/reference/grpc-api-v2/status_codes
+            Status status = Status.FAILED_PRECONDITION.withDescription("balance < amount");
+            responseObserver.onError(status.asRuntimeException());
+
+            return;
+        }
+
+        for (int i = 0; i < (amount / 10); i++) {
+            Money money = Money.newBuilder().setValue(10).build();
+            responseObserver.onNext(money);
+            AccountDatabase.deductBalance(accountNumber, 10);
+        }
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<DepositRequest> cashDeposit(StreamObserver<Balance> responseObserver) {
+        return new CashStreamingRequest(responseObserver);
+    }
+}
